78bb7145acebcf965370d3eaa31609b26b8d6016
crislaez <crislaez30@gmail.com>
2020-03-21 20:32:16 +0100

    cambios

--- src/index.js
+++ src/index.js
@@ -3,22 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import firebase from 'firebase'
 // import * as serviceWorker from './serviceWorker';
-// apiKey: "AIzaSyALQqPo3ZYGgLWhGWNl4r1-DpdHSLTwIm4",
-// authDomain: "webcrislaez-58653.firebaseapp.com",
-// databaseURL: "https://webcrislaez-58653.firebaseio.com",
-// projectId: "webcrislaez-58653",
-// storageBucket: "webcrislaez-58653.appspot.com",
-// messagingSenderId: "177846152420",
-// appId: "1:177846152420:web:b21c4de91064af557110f1"
+
+
 firebase.initializeApp({
-    apiKey: "AIzaSyAz3-bdTEstYnt_BscQ_RBwlG7bppaLKrs",
-    authDomain: "projectodatos.firebaseapp.com",
-    databaseURL: "https://projectodatos.firebaseio.com",
-    projectId: "projectodatos",
-    storageBucket: "projectodatos.appspot.com",
-    messagingSenderId: "731572375265",
-    appId: "1:731572375265:web:33577829d783474db731a0",
-    measurementId: "G-H749EBEDRX"
+
 });
 ReactDOM.render(<App />, document.getElementById('root'));
 
